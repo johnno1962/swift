b1f9c0f24bcfdfb0ad4b0e797d51b8f7b0220d38
John Holdsworth <github@johnholdsworth.com>
2019-01-14 11:59:20 +0000

    Backdoor for "Option 2"

--- lib/IRGen/GenMeta.cpp
+++ lib/IRGen/GenMeta.cpp
@@ -4128,6 +4128,7 @@ SpecialProtocol irgen::getSpecialProtocolID(ProtocolDecl *P) {
   case KnownProtocolKind::ExpressibleByStringLiteral:
   case KnownProtocolKind::ExpressibleByNilLiteral:
   case KnownProtocolKind::ExpressibleByUnicodeScalarLiteral:
+  case KnownProtocolKind::EnableExpressingIntegerByUnicodeScalar:
   case KnownProtocolKind::ExpressibleByColorLiteral:
   case KnownProtocolKind::ExpressibleByImageLiteral:
   case KnownProtocolKind::ExpressibleByFileReferenceLiteral:
