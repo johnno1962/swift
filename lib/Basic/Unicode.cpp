808702d99ca17a57472f7aada17b16c7d53f8ce1
John Holdsworth <github@johnholdsworth.com>
2019-03-20 22:23:27 +0000

    Have compiler find first grapheme using ICU.

--- lib/Basic/Unicode.cpp
+++ lib/Basic/Unicode.cpp
@@ -16,34 +16,7 @@
 
 using namespace swift;
 
-// HACK: Allow support for many newer emoji by overriding behavior of ZWJ and
-// emoji modifiers. This does not make the breaks correct for any version of
-// Unicode, but shifts the ways in which it is incorrect to be less harmful.
-//
-// TODO: Remove this hack and reevaluate whether we should have any static
-// notion of what a grapheme is.
-//
-// Returns true if lhs and rhs shouldn't be considered as having a grapheme
-// break between them. That is, whether we're overriding the behavior of the
-// hard coded Unicode 8 rules surrounding ZWJ and emoji modifiers.
-static inline bool graphemeBreakOverride(llvm::UTF32 lhs, llvm::UTF32 rhs) {
-  // Assume ZWJ sequences produce new emoji
-  if (lhs == 0x200D) {
-    return true;
-  }
-
-  // Permit continuing regional indicators
-  if (rhs >= 0x1F3FB && rhs <= 0x1F3FF) {
-    return true;
-  }
-
-  // Permit emoji tag sequences
-  if (rhs >= 0xE0020 && rhs <= 0xE007F) {
-    return true;
-  }
-
-  return false;
-}
+#include "../stdlib/public/stubs/UnicodeNormalization.cpp"
 
 StringRef swift::unicode::extractFirstExtendedGraphemeCluster(StringRef S) {
   // Extended grapheme cluster segmentation algorithm as described in Unicode
@@ -51,42 +24,23 @@ StringRef swift::unicode::extractFirstExtendedGraphemeCluster(StringRef S) {
   if (S.empty())
     return StringRef();
 
-  const llvm::UTF8 *SourceStart =
-    reinterpret_cast<const llvm::UTF8 *>(S.data());
+  auto err = __swift_stdlib_U_ZERO_ERROR;
+  auto newUBreakIterator = __swift_stdlib_ubrk_open(
+      /*type:*/ __swift_stdlib_UBRK_CHARACTER, /*locale:*/ nullptr,
+      /*text:*/ nullptr, /*textLength:*/ 0, /*status:*/ &err);
+  assert(err && "__swift_stdlib_ubrk_open fails");
 
-  const llvm::UTF8 *SourceNext = SourceStart;
-  llvm::UTF32 C[2];
-  llvm::UTF32 *TargetStart = C;
+  auto newUText = __swift_stdlib_utext_openUTF8(
+      /*ut:*/ nullptr, /*s:*/ S.data(), /*len:*/ S.size(), /*status:*/ &err);
+  assert(err && "__swift_stdlib_utext_openUTF8 fails");
 
-  ConvertUTF8toUTF32(&SourceNext, SourceStart + S.size(), &TargetStart, C + 1,
-                     llvm::lenientConversion);
-  if (TargetStart == C) {
-    // The source string contains an ill-formed subsequence at the end.
-    return S;
-  }
+  __swift_stdlib_ubrk_setUText(newUBreakIterator, newUText, &err);
+  assert(err && "__swift_stdlib_ubrk_setUText fails");
 
-  GraphemeClusterBreakProperty GCBForC0 = getGraphemeClusterBreakProperty(C[0]);
-  while (true) {
-    size_t C1Offset = SourceNext - SourceStart;
-    ConvertUTF8toUTF32(&SourceNext, SourceStart + S.size(), &TargetStart, C + 2,
-                       llvm::lenientConversion);
+  StringRef out(S.data(), __swift_stdlib_ubrk_following(newUBreakIterator, 0));
+  __swift_stdlib_ubrk_close(newUBreakIterator);
 
-    if (TargetStart == C + 1) {
-      // End of source string or the source string contains an ill-formed
-      // subsequence at the end.
-      return S.slice(0, C1Offset);
-    }
-
-    GraphemeClusterBreakProperty GCBForC1 =
-        getGraphemeClusterBreakProperty(C[1]);
-    if (isExtendedGraphemeClusterBoundary(GCBForC0, GCBForC1) &&
-        !graphemeBreakOverride(C[0], C[1]))
-      return S.slice(0, C1Offset);
-
-    C[0] = C[1];
-    TargetStart = C + 1;
-    GCBForC0 = GCBForC1;
-  }
+  return out;
 }
 
 static bool extractFirstUnicodeScalarImpl(StringRef S, unsigned &Scalar) {
